ed05ec029dd707712589157e49e996829da182c5
MADTeacher <madmasm@gmail.com>
2020-10-28 19:50:44 +0300

    add L3

---
+++ Lesson 3/Lesson3 - doc/main_dir.py
@@ -0,0 +1,9 @@
+import testdoc
+
+if __name__ == '__main__':
+    print(testdoc.__doc__)
+    print("=" * 20)
+    print(testdoc.square.__doc__)
+    print("=" * 20)
+    print(testdoc.Employee.__doc__)
+
---
+++ Lesson 3/Lesson3 - doc/main_help.py
@@ -0,0 +1,5 @@
+import testdoc
+
+if __name__ == '__main__':
+    print(help(testdoc))
+
